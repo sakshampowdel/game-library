ad6921779306d00b5cdb8672241261aa87f10b92
Saksham Powdel <sakshampowdel@gmail.com>
2025-12-14 21:09:09 -0500

    feat: add GET and POST games

---
+++ backend/Dtos/CreateGameDto.cs
@@ -0,0 +1,8 @@
+namespace backend.Dtos;
+
+public record class CreateGameDto(
+  string Name,
+  string Genre,
+  decimal Price,
+  DateOnly ReleaseDate
+);
--- backend/Program.cs
+++ backend/Program.cs
@@ -1,6 +1,70 @@
+using backend.Dtos;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+const string GetGameEndpointName = "GetGame";
+
+List<GameDto> games = [
+  new (
+    1,
+    "Civilization VI",
+    "4X",
+    59.99m,
+    new DateOnly(2016, 10, 21)
+    ),
+  new (
+    2,
+    "Stellaris",
+    "4X",
+    39.99m,
+    new DateOnly(2016, 5, 9)
+    ),
+  new (
+    3,
+    "Cities: Skylines",
+    "Management",
+    29.99m,
+    new DateOnly(2015, 3, 10)
+    ),
+  new (
+    4,
+    "Planet Zoo",
+    "Management",
+    44.99m,
+    new DateOnly(2019, 11, 5)
+    ),
+  new (
+    5,
+    "Endless Legend",
+    "4X",
+    29.99m,
+    new DateOnly(2014, 9, 18)
+    )
+
+];
+
+// GET /games
+app.MapGet("games", () => games);
+
+// GET /games/1
+app.MapGet("games/{id}", (int id) => games.Find(game => game.Id == id))
+  .WithName(GetGameEndpointName);
+
+// POST /games
+app.MapPost("games", (CreateGameDto newGame) =>
+{
+  GameDto game = new (
+    games.Count + 1,
+    newGame.Name,
+    newGame.Genre,
+    newGame.Price,
+    newGame.ReleaseDate
+  );
+
+  games.Add(game);
+
+  return Results.CreatedAtRoute(GetGameEndpointName, new { id = game.Id }, game);
+});
 
 app.Run();
